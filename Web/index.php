dbbb1ce5ff1d927d4b5b1393348544452498037f
ajlopez <webmaster@ajlopez.com>
2013-01-01 10:51:25 -0300

    Properties and images at index, WIP

--- Web/index.php
+++ Web/index.php
@@ -1,18 +1,33 @@
 <?
-	$Page->Title = 'Main';
+	$Page->Title = 'Inmob';
 	include_once('./Security.inc.php');
 	include_once($Page->Prefix.'includes/Header.inc.php');
+	include_once($Page->Prefix . 'ajfwk/Database.inc.php');
+	DbConnect();
+
+    $sql = "select prop.Id, prop.Nombre, prop.Domicilio, prop.IdTipo, tipo.Nombre as TipoNombre, imagen.NombreArchivo, imagen.Uuid from $Cfg[SqlPrefix]propiedades as prop left join $Cfg[SqlPrefix]propiedadimagenes as imagen on prop.Id = imagen.IdPropiedad , $Cfg[SqlPrefix]tipospropiedad as tipo where prop.IdTipo = tipo.Id and imagen.Principal = 1 order by prop.Id desc";
+    $rs = DbExecuteQuery($sql);
 ?>
 
 <div>
-Site example, using <a href='http://ajgenesis.codeplex.com'>AjGenesis</a>,
- <a href="http://www.php.net">PHP</a> and <a href="http://www.mysql.com">MySql</a>.
+<?= $sql ?>
+</div>
+<?
+	while ($reg=DbNextRow($rs)) {
+?>
+<div class='propitem'>
+<div class='propname'>
 </div>
 <div>
-Created by <a href="http://www.ajlopez.com">www.ajlopez.com</a>.
+<img src="<?= $Page->Prefix ?>images/photos/<?= $reg['Uuid'] . '.' . pathinfo($reg['NombreArchivo'], PATHINFO_EXTENSION)?>" border="0" width="200"/>
 </div>
-
+</div>
+<?
+    }
+?>
 
 <?
+    DbFreeResult($rs);
+	DbDisconnect();
 	include_once('includes/Footer.inc.php');
 ?>
