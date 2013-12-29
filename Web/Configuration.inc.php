0453e7e9894a72cc6726f3b941c41a0d4a05768d
ajlopez <webmaster@ajlopez.com>
2013-12-29 09:16:20 -0300

    Fix Configuration values

--- Web/Configuration.inc.php
+++ Web/Configuration.inc.php
@@ -1,3 +1,5 @@
 <?php
-    error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE & ~E_WARNING);
+    // http://php.net/manual/en/errorfunc.constants.php
+    // error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE & ~E_WARNING);
+    error_reporting(32767 & ~16384 & ~2048 & ~8 & ~2);
 ?>
