87a7dba5d6bc4e8f2b62a463c7a88fb9d189e962
ajlopez <webmaster@ajlopez.com>
2014-01-15 08:33:57 -0300

    Adding enumerations, fixing logout

--- Web/consorcios/includes/Enumerations.inc.php
+++ Web/consorcios/includes/Enumerations.inc.php
@@ -1,3 +1,10 @@
 <?php
 
+$EnumOperacion = array(
+      1 => 'Venta',
+      2 => 'Alquiler',
+      3 => 'Compra',
+      4 => 'Alquiler Temporario',
+      5 => 'Tiempo Compartido');
+
 ?>
--- Web/includes/Enumerations.inc.php
+++ Web/includes/Enumerations.inc.php
@@ -3,6 +3,8 @@
 $EnumOperacion = array(
       1 => 'Venta',
       2 => 'Alquiler',
-      3 => 'Compra');
+      3 => 'Compra',
+      4 => 'Alquiler Temporario',
+      5 => 'Tiempo Compartido');
 
 ?>
